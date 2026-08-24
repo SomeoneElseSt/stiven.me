02897a07f2f5844fd5ffce9f6086dc884afebcca
Stiven <Strnmrtnz@icloud.com>
2026-08-24 18:23:02 -0400

    feat: ammend console output to include stiven.me/hi api info

--- src/init.ts
+++ src/init.ts
@@ -4,6 +4,17 @@ import { initPrefetch } from './prefetch.js';
 import { addSocialLinkClickListeners } from './listeners.js';
 import { addNpProgressListeners } from './nprogress.js';
 
+console.log(`
+================================================
+|                                              |
+|  Hello, curious mind.                        |
+|                                              |
+|  If you really are thaaat curious, POST      |
+|  your "name", "contact", and "note" to       |
+|  stiven.me/api/hi                            |
+|                                              |
+================================================`);
+
 function initializeApp(): void {
     initLocaleSwitcher();
     if (!shouldDisableNProgress()) {
